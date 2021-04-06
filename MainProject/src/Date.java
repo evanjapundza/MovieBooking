ca8e2576946a883e3a8e8aed73f790e242d88952
mgoumbal <mgoumbal@iu.edu>
2021-04-06 17:03:15 -0400

    Skeleton code: Account,Date,User (MG)

--- MainProject/src/Date.java
+++ MainProject/src/Date.java
@@ -1,2 +1,37 @@
 public class Date {
+
+	// instance variables
+	private int year; //year of date
+	private int month; //month of date
+	private int day; //day of date
+	
+	//Constructor
+	public Date(int year,int month, int day) {
+		//all-args constructor
+		//initializes instance variables
+	}
+	
+	//getters and setters
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public int getMonth() {
+		return month;
+	}
+	public void setMonth(int month) {
+		this.month = month;
+	}
+	public int getDay() {
+		return day;
+	}
+	public void setDay(int day) {
+		this.day = day;
+	}
+
+
+	
+	
 }
