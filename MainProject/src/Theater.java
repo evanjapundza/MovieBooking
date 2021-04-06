33d4b3a3fac80ddeb1adffd7b38a2db254e05302
cdrassel <cdrassel@iu.edu>
2021-04-06 17:07:32 -0400

    created and provided skeleton code for theater class

---
+++ MainProject/src/Theater.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class Theater
+{
+    private ArrayList<Movie> movies;
+    private String address;
+
+    public Theater(String address)
+    {
+
+    }
+
+    //return address of theater
+    public String getAddress()
+    {
+        return address;
+    }
+
+    public void setAddress()
+    {
+        //set address of theater
+    }
+
+    public void showAvailableMovies()
+    {
+        //show all movies available to be played
+    }
+
+}
